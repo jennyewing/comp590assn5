430a5057c7073c4ebfb2c660826f2afae301d6f8
Jenny Ewing <45116181+jennyewing@users.noreply.github.com>
2021-10-18 17:14:51 -0400

    Update DiningPhilosophers.java

--- assn6/src/assn6/DiningPhilosophers.java
+++ assn6/src/assn6/DiningPhilosophers.java
@@ -7,25 +7,26 @@ public class DiningPhilosophers {
         Philosopher[] philosophers = new Philosopher[5];
         Object[] forks = new Object[philosophers.length];
         
-        //Use generic Java objects to represent forks
+        // Use generic Java objects to represent forks
         for (int i = 0; i < forks.length; i++) {
             forks[i] = new Object();
         }
 
         for (int i = 0; i < philosophers.length; i++) {
-            //Designates left and right fork for each philosopher as we go through the loop 
+            // Designates left and right fork for each philosopher as we go through the loop 
             Object leftFork = forks[i];
+            // Once we get to the last two forks in the loop, forks[5] doesn't exist; it's actually forks[0] because the table is circular!
             Object rightFork = forks[(i + 1) % forks.length];
 
-            //Designate any one philosopher to pick up the right fork first (i.e. third philosopher)
-            //This step prevents deadlock by preventing the circular wait condition
+            // Designate any one philosopher to pick up the right fork first (i.e. third philosopher)
+            // This step prevents deadlock by preventing the circular wait condition
             if (i == philosophers.length - 3) {
                 philosophers[i] = new Philosopher(rightFork, leftFork); 
             } else {
                 philosophers[i] = new Philosopher(leftFork, rightFork);
             }
             
-            //Indicates which philosopher begins the new thread 
+            // Indicates which philosopher begins the new thread 
             Thread t = new Thread(philosophers[i], "Philosopher " + (i + 1));
             t.start();
         }
