9f84e55e319ec44287f31bddccefede426bde8dc
Christina Alperi <46232770+christinaalperi@users.noreply.github.com>
2021-10-18 17:01:31 -0400

    Update Philosopher.java

--- assn6/src/assn6/Philosopher.java
+++ assn6/src/assn6/Philosopher.java
@@ -25,16 +25,21 @@ public class Philosopher implements Runnable {
         try {
             while (true) {
                 // Philosopher is thinking/just has a left fork
-                doAction((double) System.nanoTime() / 1_000_000_000 + ": Think");
+		String t = ((double) System.nanoTime() / 1_000_000_000).toString();
+                doAction(t + ": Philosophizing");
                 synchronized (leftFork) { // in other class switching order of left/right fork objects changes the order of pick up to prevent deadlock
-                    doAction((double) System.nanoTime() / 1_000_000_000 + ": Pick up left fork");
+		    t = ((double) System.nanoTime() / 1_000_000_000).toString();
+                    doAction(t + ": Pick up left fork");
                     synchronized (rightFork) {
-                        // Philosopher can eat now with both forks
-                        doAction((double) System.nanoTime() / 1_000_000_000 + ": Pick up right fork and feast"); 
-                        doAction((double) System.nanoTime() / 1_000_000_000 + ": Put down right fork");
+                        // Philosopher can eat now with both fork
+			t = ((double) System.nanoTime() / 1_000_000_000).toString();
+                        doAction(t + ": Pick up right fork and feast"); 
+		        t = ((double) System.nanoTime() / 1_000_000_000).toString();
+                        doAction(t + ": Put down right fork");
                     }
                     // Philosopher is back to thinking/has put down a left fork for another to pick up
-                    doAction((double) System.nanoTime() / 1_000_000_000 + ": Put down left fork and think");
+		    t = ((double) System.nanoTime() / 1_000_000_000).toString();
+                    doAction(t + ": Put down left fork and philosophize some more");
                 }
             }
         } catch (InterruptedException e) { 
